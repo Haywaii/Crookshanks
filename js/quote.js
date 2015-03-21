3498196e409fa14cdd7d80c4c08a63a5e2fa69ce
Emmanuel <hbmanu@gmail.com>
2015-03-21 18:39:49 +0100

    Update MAJ Last meeting

--- js/quote.js
+++ js/quote.js
@@ -3,11 +3,11 @@
  $(document).ready(function(){
       var myQuotes = new Array();
       myQuotes[0] = "&ldquo; Cats will turn into Lyon &bdquo;";
-      myQuotes[1] = "&ldquo; Nothing’s difficult, everything’s a challenge &bdquo;";
+      //myQuotes[1] = "&ldquo; Nothing’s difficult, everything’s a challenge &bdquo;";
 	  
 	  var mySource = new Array();
       mySource[0] = "Crookshanks Lyon Motto";
-      mySource[1] = "Turskee Air Force Motto";
+      //mySource[1] = "Turskee Air Force Motto";
       
       var myRandom = Math.floor(Math.random()*myQuotes.length);
       $(".message").html(myQuotes[myRandom]);
